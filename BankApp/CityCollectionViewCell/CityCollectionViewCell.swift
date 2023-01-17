241b3f6dd3f1902c95eb69c3bbe826ba851247ab
Nikita <55090468+banger980@users.noreply.github.com>
2023-01-17 22:00:58 +0300

    CreateSortCollectionViewAndSortCity

--- BankApp/CityCollectionViewCell/CityCollectionViewCell.swift
+++ BankApp/CityCollectionViewCell/CityCollectionViewCell.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class CityCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var cityButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func setCityButton(name: String) {
+        cityButton.setTitle(name, for: .normal)
+    }
 }
