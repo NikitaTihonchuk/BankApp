50712b8791607fb2a55a6213d42523beb358a287
Nikita <55090468+banger980@users.noreply.github.com>
2023-01-17 20:32:08 +0300

    CreateViewCell

---
+++ BankApp/SortCollectionViewCell/SortCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SortCollectionViewCell.swift
+//  BankApp
+//
+//  Created by Nikita on 17.01.23.
+//
+
+import UIKit
+
+class SortCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
